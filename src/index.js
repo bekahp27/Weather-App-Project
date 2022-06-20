f9b399deb5762c88ab3fb51d2ccaadcf57923236
bekahp27 <107060647+bekahp27@users.noreply.github.com>
2022-06-20 22:07:51 +0100

    Forcast JS removed

--- src/index.js
+++ src/index.js
@@ -21,42 +21,6 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForcast() {
-  let forcastElement = document.querySelector("#weather-forcast");
-
-  let forcastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forcastHTML =
-      forcastHTML +
-      `  
-      <div class="col-2">
-        <div class="weather-forcast-date">${day}</div>
-        <img
-          src="images/Partly Cloudy.png"
-          alt="Partly Cloudy"
-          class="forcast-icdon"
-          id="icon"
-          width="30px"
-        />
-        <div class="weather-forcast-temperatures">
-          <span class="weather-forcast-temperature-max"> 18° </span>
-          <span class="weather-forcast-temperature-min"> 10° </span>
-        </div>
-      </div>
-    `;
-  });
-
-  forcastHTML = forcastHTML + `</div>`;
-  forcastElement.innerHTML = forcastHTML;
-}
-function getForcast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "c8bcf38f16acb69ddfc1b3daa6ad1ae3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&long=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForcast);
-}
-
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#currentTemp");
   let cityElement = document.querySelector("#currentCityHeader");
@@ -79,8 +43,6 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForcast(response.data.coord);
 }
 
 function search(city) {
@@ -124,4 +86,3 @@ let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Shaftesbury");
-displayForcast();
